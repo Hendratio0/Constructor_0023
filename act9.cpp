718a97981882f66aeca819556270e2bd223a6aa8
Hendratio0 <hendraaatio@gmail.com>
2025-05-17 13:48:55 +0700

    commit4

--- act9.cpp
+++ act9.cpp
@@ -22,3 +22,14 @@ int main()
     cout << bukunya.setJudul("Matematika").getJudul();
     return 0;
 }
+
+// destruktor
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+}
